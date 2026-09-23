9820ff18c5cc04ef1cf7b57c78fe54ba5773f08a
vinicius  Hoffelder Colussi <viniciuschoffelder1@gmail.com>
2026-09-22 21:20:25 -0300

    Completar o nome dos integrantes em src/data/grupo.js

--- src/data/grupo.js
+++ src/data/grupo.js
@@ -1,8 +1,23 @@
 const grupo = [
   {
     id: 1,
-    nome: "Vinicius - informe o nome completo",
-    ra: "Informe o RA",
+    nome: "Vinicius Hoffelder",
+    ra: "1137833",
+  },
+  {
+    id: 2,
+    nome: "Eduardo Barreda",
+    ra: "1138704",
+  },
+  {
+    id: 3,
+    nome: "Joao Buratti",
+    ra: "1136821",
+  },
+  {
+    id: 4,
+    nome: "Victor Quadri",
+    ra: "1136643",
   },
 ];
 
